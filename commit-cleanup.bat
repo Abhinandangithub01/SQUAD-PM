520211d3dca59167a0ad7c62b5492af0fb936bb7
Abhinandan <mail2abhinandan01@gmail.com>
2025-10-23 02:13:53 +0530

    chore: remove outdated documentation and cleanup files

--- commit-cleanup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git commit -m "chore: Remove unnecessary documentation files"
-git push origin main
-echo.
-echo Cleanup committed and pushed!
-pause
--- commit-fixes.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git commit -m "fix: Add PlusIcon import and configure ESLint rules"
-git push codecommit main
-git push origin main
-echo Build errors fixed!
-pause
